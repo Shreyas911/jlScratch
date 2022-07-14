f0e78c998d971012026b20dd4282d0dff23ccbc4
Michel Schanen <michel.schanen@gmail.com>
2022-07-14 11:30:41 -0600

    Make Enzyme not crash

--- mpi_mt_glacier.jl
+++ mpi_mt_glacier.jl
@@ -11,7 +11,7 @@ using MPI
 	return h
 end
 
-function forward_problem(V_local::AbstractArray , xx::AbstractArray, nx::Int, dx::Float64, xend::Float64, dt::Float64, tend::Float64, AT)
+function forward_problem(V_local::AbstractArray , xx::AbstractArray, nx::Int, dx::Float64, xend::Float64, dt::Float64, tend::Float64, xarr::Vector{Float64}, i1::Int, i2::Int)
 	rho = 920.0
 	g = 9.2
 	n = 3
@@ -24,27 +24,23 @@ function forward_problem(V_local::AbstractArray , xx::AbstractArray, nx::Int, dx
 	M1 = 0.0002
 	nt = Int(round(tend/dt))
 
-	rank = MPI.Comm_rank(comm)
-	size = MPI.Comm_size(comm)
-
-	# One step in time for one part of the domainor one thread 
-	i1 = Int(round(rank / size * (nx+size))) - rank + 1
-	i2 = Int(round((rank + 1) / size * (nx+size))) - rank
+	rank = MPI.Comm_rank(MPI.COMM_WORLD)
+	size = MPI.Comm_size(MPI.COMM_WORLD)
 
 	nx_local = i2-i1
 
-	print("Rank $(rank) of $(size) with domain limits $(i1), $(i2) and nx_local $(nx_local)\n")
+	# print("Rank $(rank) of $(size) with domain limits $(i1), $(i2) and nx_local $(nx_local)\n")
 
 	h = zeros((nx_local+1, nt+1))
 	h_capital = zeros((nx_local+1, nt+1))
 
 	D = zeros(nx_local)
 	phi = zeros(nx_local)
-	xarr = [(i-1)*dx for i in i1:i2]
-	M = zeros(nx_local+1)
-	b = zeros(nx_local+1)
 
-	M .= M0 .- xarr .* M1 .+ xx[i1:i2]
+	b = zeros(nx_local+1)
+	x_local = xx[i1:i2]
+
+	M = M0 .- xarr .* M1 .+ x_local
 	b .= 1.0 .+ bx .* xarr
 
 	if rank == 0
@@ -58,7 +54,7 @@ function forward_problem(V_local::AbstractArray , xx::AbstractArray, nx::Int, dx
 	h[:,1] .= ones(nx_local+1) .* b
 	h_capital[:,1] .= h[:,1] .- b
 
-	@inbounds for t in 1:nt
+	for t in 1:nt
 
 		D .= C .* ((h_capital[1:nx_local,t] .+ h_capital[2:nx_local+1,t]) ./ 2.0).^(n+2) .* ((h[2:nx_local+1,t] .- h[1:nx_local,t]) ./ dx).^(n-1)
 		phi .= -D .* (h[2:nx_local+1,t] .- h[1:nx_local,t]) ./ dx
@@ -77,8 +73,8 @@ function forward_problem(V_local::AbstractArray , xx::AbstractArray, nx::Int, dx
 			if rank == size-1
 
 				fill!(send_mesg_left, phi[1])
-				sreq_left = MPI.Send(send_mesg_left, rank-1, 100 + rank, comm)
-				rreq_left = MPI.Recv!(recv_mesg_left, rank-1, 100 + rank - 1, comm)
+				sreq_left = MPI.Send(send_mesg_left, rank-1, 100 + rank, MPI.COMM_WORLD)
+				rreq_left = MPI.Recv!(recv_mesg_left, rank-1, 100 + rank - 1, MPI.COMM_WORLD)
 
 				h[1,t+1] = h[1,t] + M[1] * dt - dt/dx * (phi[1] - recv_mesg_left[1])
 				h[1,t+1] = update_h(h[1,t+1], b[1])
@@ -87,8 +83,8 @@ function forward_problem(V_local::AbstractArray , xx::AbstractArray, nx::Int, dx
 			elseif rank == 0
 
 				fill!(send_mesg_right, phi[nx_local])
-				sreq_right = MPI.Send(send_mesg_right, rank+1, 100 + rank, comm)
-				rreq_right = MPI.Recv!(recv_mesg_right, rank+1, 100 + rank + 1, comm)
+				sreq_right = MPI.Send(send_mesg_right, rank+1, 100 + rank, MPI.COMM_WORLD)
+				rreq_right = MPI.Recv!(recv_mesg_right, rank+1, 100 + rank + 1, MPI.COMM_WORLD)
 
 				h[nx_local+1,t+1] = h[nx_local+1,t] + M[nx_local+1] * dt - dt/dx * (recv_mesg_right[1] - phi[nx_local])
 				h[nx_local+1,t+1] = update_h(h[nx_local+1,t+1], b[nx_local+1])
@@ -97,26 +93,26 @@ function forward_problem(V_local::AbstractArray , xx::AbstractArray, nx::Int, dx
 			else
 
 				fill!(send_mesg_right, phi[nx_local])
-				sreq_right = MPI.Send(send_mesg_right, rank+1, 100 + rank, comm)
-				rreq_right = MPI.Recv!(recv_mesg_right, rank+1, 100 + rank + 1, comm)
+				sreq_right = MPI.Send(send_mesg_right, rank+1, 100 + rank, MPI.COMM_WORLD)
+				rreq_right = MPI.Recv!(recv_mesg_right, rank+1, 100 + rank + 1, MPI.COMM_WORLD)
 
 				h[nx_local+1,t+1] = h[nx_local+1,t] + M[nx_local+1] * dt - dt/dx * (recv_mesg_right[1] - phi[nx_local])
 				h[nx_local+1,t+1] = update_h(h[nx_local+1,t+1], b[nx_local+1])
 				h_capital[nx_local+1,t+1] = h[nx_local+1,t+1] - b[nx_local+1]
 
 				fill!(send_mesg_left, phi[1])
-				sreq_left = MPI.Send(send_mesg_left, rank-1, 100 + rank, comm)
-				rreq_left = MPI.Recv!(recv_mesg_left, rank-1, 100 + rank - 1, comm)
+				sreq_left = MPI.Send(send_mesg_left, rank-1, 100 + rank, MPI.COMM_WORLD)
+				rreq_left = MPI.Recv!(recv_mesg_left, rank-1, 100 + rank - 1, MPI.COMM_WORLD)
 
 				h[1,t+1] = h[1,t] + M[1] * dt - dt/dx * (phi[1] - recv_mesg_left[1])
 				h[1,t+1] = update_h(h[1,t+1], b[1])
-				h_capital[1,t+1] = h[1,t+1] - b[1]	
+				h_capital[1,t+1] = h[1,t+1] - b[1]
 
-			end		
+			end
 
 		end
 
-		MPI.Barrier(comm)
+	# 	MPI.Barrier(MPI.COMM_WORLD)
 
 	end
 
@@ -140,8 +136,8 @@ function forward_problem(V_local::AbstractArray , xx::AbstractArray, nx::Int, dx
 		# 	for i in 1:size-1
 		# 		rreq = MPI.Recv!(recv_mesg, i, 200 + i, comm)
 		# 		V_local = V_local + recv_mesg[1]
-		# 	end 
-		# end 
+		# 	end
+		# end
 
 	else
 
@@ -149,14 +145,11 @@ function forward_problem(V_local::AbstractArray , xx::AbstractArray, nx::Int, dx
 
 	end
 
-	# V_local[1] = MPI.Allreduce(V_local[1], MPI.SUM, comm)
-
 	return
 
 end
 
 MPI.Init()
-comm = MPI.COMM_WORLD
 
 dx = 1.0
 xend = 30.0
@@ -165,16 +158,29 @@ tend = 100.0
 
 nx = Int(round(xend/dx))
 xx = zeros(nx+1)
-∂V_∂xx=zero(xx)
+∂V_∂xx=zeros(nx+1)
 V = [0.0]
-forward_problem(V, xx,nx,dx,xend,dt,tend, Array)
+rank = MPI.Comm_rank(MPI.COMM_WORLD)
+size = MPI.Comm_size(MPI.COMM_WORLD)
 
-print("$(MPI.Comm_rank(comm)), V = $(V)\n")
+# One step in time for one part of the domainor one thread
+i1 = Int(round(rank / size * (nx+size))) - rank + 1
+i2 = Int(round((rank + 1) / size * (nx+size))) - rank
+xarr = [(i-1)*dx for i in i1:i2]
+forward_problem(V, xx, nx, dx, xend, dt, tend, xarr, i1, i2)
+MPI.Reduce!(V, MPI.SUM, 0, MPI.COMM_WORLD)
+if rank == 0
+	print("V = $(V)\n")
+end
 
 dV = [1.0]
 V = [0.0]
-autodiff(forward_problem, Duplicated(V, dV), Duplicated(xx, ∂V_∂xx), nx, dx, xend, dt, tend, Array)
+autodiff(forward_problem, Duplicated(V, dV), Duplicated(xx, ∂V_∂xx), nx, dx, xend, dt, tend, xarr, i1, i2)
+MPI.Reduce!(V, MPI.SUM, 0, MPI.COMM_WORLD)
+MPI.Reduce!(∂V_∂xx, MPI.SUM, 0, MPI.COMM_WORLD)
 
-print("$(MPI.Comm_rank(comm)), ∂V_∂xx = $(∂V_∂xx)\n")
+if rank == 0
+	print("V = $V, ∂V_∂xx = $(∂V_∂xx)\n")
+end
 
 MPI.Finalize()
